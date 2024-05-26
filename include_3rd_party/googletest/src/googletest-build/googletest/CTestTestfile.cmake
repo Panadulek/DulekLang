9a9df1e635d930874913e0b0e45d88e25d2fbfb4
jordan <jazborj@gmail.com>
2024-05-26 02:30:40 +0200

    remove googletest lib from repo

--- include_3rd_party/googletest/src/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/LocalRepo/Compilers/DulekLang/include_3rd_party/googletest/src/googletest/googletest
-# Build directory: E:/LocalRepo/Compilers/DulekLang/include_3rd_party/googletest/src/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
